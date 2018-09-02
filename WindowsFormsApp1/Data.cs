6437630cb3d9cf326dbe3c6b5b380dd7585a042e
Scorpion8754 <35542934+Scorpion8754@users.noreply.github.com>
2018-09-01 18:18:36 -0700

    first commit

---
+++ WindowsFormsApp1/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    [Serializable]
+    public class Data
+    {
+        private string Name;
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        private string Phone;
+        public string phone
+        {
+            get { return Phone; }
+            set { Phone = value; }
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -10,11 +13,77 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+
     public partial class Form1 : Form
     {
+        public static string dir = SelectFile();
+        
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Data data = new Data
+            {
+                name = textBox1.Text,
+                phone = textBox2.Text
+            };
+            BinaryFormatter bf = new BinaryFormatter();
+
+            FileStream fsout = new FileStream(dir, FileMode.Create, FileAccess.Write, FileShare.None);
+                try
+            {
+                using (fsout)
+                {
+                    bf.Serialize(fsout, data);
+                    label6.Text = "Object Serialized";
+                }
+            }
+            catch
+            {
+                label6.Text = "Error";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            Data data = new Data();
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            FileStream fsin = new FileStream(@dir, FileMode.Open, FileAccess.Read, FileShare.None);
+            try
+            {
+                using (fsin)
+                {
+                    data = (Data)bf.Deserialize(fsin);
+                    label6.Text = "Object Derserialized";
+                    textBox1.Text = data.name;
+                    textBox2.Text = data.phone;
+                }
+            }
+            catch
+            {
+                label6.Text = "ERROR";
+            }
+        }
+
+        private static string SelectFile()
+        {
+            Console.WriteLine(dir);
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                return (ofd.ToString());
+                
+            else
+            {
+                return "null";
+            }
+            
         }
     }
 }
